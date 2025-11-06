c4f4d54ad3fc426fb3306bf8f4a7b36e7c5d9aed
Oluwayomi Temidayo <temidayoayomide360@gmail.com>
2025-11-06 19:11:30 +0100

    updates

---
+++ c-introduction/quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("The future belongs to those\n");
+    printf("who believe in the beauty\n");
+    printf("of their dreams.\n");
+    return 0;
+}
