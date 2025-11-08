fa0bc8f0f78322976c6ab2d6578bf4aafada47e8
Oluwayomi Temidayo <temidayoayomide360@gmail.com>
2025-11-08 20:53:04 +0100

    Updated project description

--- c-variables-data_types/hello.c
+++ c-variables-data_types/hello.c
@@ -1,6 +1,14 @@
-/**
- * main - the main function
- * description: The entry point of our C program
- * return: 0
- */
-//This project covers the basics of the C variables, Data types, including history, importance, structure, and simple programs.
+// This program prints a greeting message in C.
+
+/*
+C is still important today because it’s fast, efficient, 
+and forms the foundation for many modern languages like C++, Java, and Python.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, C programming!\n");
+    return 0;
+}
