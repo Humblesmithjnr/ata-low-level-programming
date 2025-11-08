86efbf9d892c41dab58075ce76619e89522e21de
Oluwayomi Temidayo <temidayoayomide360@gmail.com>
2025-11-08 21:37:13 +0100

    Updated project description

---
+++ c-variables-data_types/compilation_demo.c
@@ -0,0 +1,14 @@
+// This program prints a greeting message in C.
+
+/*
+C is still important today because it’s fast, efficient, 
+and forms the foundation for many modern languages like C++, Java, and Python.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Compilation in C works!\n");
+    return 0;
+}
