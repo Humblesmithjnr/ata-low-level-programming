814989a1059ffc01330a22ff3d6e5ba687355396
Oluwayomi Temidayo <temidayoayomide360@gmail.com>
2025-11-08 17:46:38 +0100

    Updated project description

---
+++ c-variables-data_types/README.md
@@ -0,0 +1,2 @@
+# C variables, Data types
+This project covers the basics of the C variables, Data types, including history, importance, structure, and simple programs.
--- c-variables-data_types/hello.c
+++ c-variables-data_types/hello.c
@@ -3,3 +3,4 @@
  * description: The entry point of our C program
  * return: 0
  */
+//This project covers the basics of the C variables, Data types, including history, importance, structure, and simple programs.
